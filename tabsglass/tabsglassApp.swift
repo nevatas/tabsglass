759de7e268c2a4c882ae90c8c86ca35cc66f44b6
LibGDX Template <libgdx@template.com>
2026-01-22 02:25:04 +0500

    Initial Commit

---
+++ tabsglass/tabsglassApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabsglassApp.swift
+//  tabsglass
+//
+//  Created by Sergey Tokarev on 22.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct tabsglassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tabsglassTests/tabsglassTests.swift
@@ -0,0 +1,17 @@
+//
+//  tabsglassTests.swift
+//  tabsglassTests
+//
+//  Created by Sergey Tokarev on 22.01.2026.
+//
+
+import Testing
+@testable import tabsglass
+
+struct tabsglassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
